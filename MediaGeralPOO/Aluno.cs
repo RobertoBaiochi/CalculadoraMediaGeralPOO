41f5634a3c75bdd4e27372a541208b6b38445f21
RobertoBaiochi <robertombbaiocho@gmail.com>
2024-03-28 23:09:14 -0300

    feat: calculadora

---
+++ MediaGeralPOO/Aluno.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+
+namespace MediaGeralPOO
+{
+    internal class Aluno
+    {
+        public string Nome { get; private set; }
+
+        private double[] _notas;
+
+        public double Media
+        {
+            get
+            {
+                return CalcularMedia();
+            }
+            
+        }
+
+        public Aluno(string nome, int provas)
+        {
+            Nome = nome;
+            _notas = new double[provas];
+        }
+
+        public void InserirNotas()
+        {
+            for (int i = 0; i < _notas.Length; i++)
+            {
+                Console.Write("Digite a nota da prova #" + (i + 1) + ": ");
+                _notas[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+        }
+
+        public double CalcularMedia()
+        {
+            double media = 0;
+            
+            foreach (double nota in _notas) 
+            { 
+                media += nota;
+            }
+
+            return media;
+        }
+
+    }
+}
---
+++ MediaGeralPOO/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Globalization;
+
+namespace MediaGeralPOO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "# Calcular Media Total #";
+
+            Console.Write("Digite a Quantidade de Alunos: ");
+            int nAlunos = int.Parse(Console.ReadLine());
+
+            Aluno[] alunos = new Aluno[nAlunos];
+
+            for (int i = 0; i < alunos.Length; i++)
+            {
+                Console.Clear();
+                Console.Write("Digite o Nome do Aluno #" + (i + 1) + ": ");
+                string nome = Console.ReadLine();
+
+                Console.Write("Digite a quantidade de Provas do Aluno #" + (i + 1) + ": ");
+                int provas = int.Parse(Console.ReadLine());
+
+                alunos[i] = new Aluno(nome, provas);
+
+                Console.WriteLine("Digite as notas do aluno " + nome);
+                alunos[i].InserirNotas();
+            }
+
+            Console.Clear();
+
+            double mediaGeral = 0;
+
+            foreach (Aluno aluno in alunos)
+            {
+                Console.WriteLine("Nome: " + aluno.Nome);
+                Console.WriteLine("Media: " + aluno.Media.ToString("F2", CultureInfo.InvariantCulture));
+
+                mediaGeral += aluno.Media;
+            }
+
+            double resultadoFinal = mediaGeral / alunos.Length;
+
+            Console.WriteLine("A media geral é: " + resultadoFinal.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
